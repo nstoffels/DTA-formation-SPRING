1bfb81a77c5b64e951ccc4946e83a405d6bcdc33
nstoffels <n.stoffels@groupe-esigelec.fr>
2015-07-27 11:10:09 +0200

    septième tp spring, les AOP

--- src/main/java/com/bankonet/aspect/LogAspect.java
+++ src/main/java/com/bankonet/aspect/LogAspect.java
@@ -3,9 +3,13 @@
  */
 package com.bankonet.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
+
+import com.mysql.jdbc.log.Log;
+import com.mysql.jdbc.log.LogFactory;
 
 
 
@@ -18,14 +22,28 @@ import org.springframework.stereotype.Component;
 public class LogAspect {
 
 	final String cut="(execution(* com.bankonet.dao.*.*(..))||execution(* com.bankonet.metier.*.*(..)))";
+	
 	/**
 	 * 
 	 */
 	@Before(cut)
-	public void LogBefore() {
+	public void LogBefore(JoinPoint joinPoint) {
 		// TODO Auto-generated constructor stub
 		System.out.println("----AOP----");
-		System.out.println("lancement d'une métode dao ou métier");
+		System.out.println(" " + joinPoint.getTarget().getClass());
+		System.out.println(" "+ joinPoint.getSignature().getName());
+		System.out.println(" " + joinPoint.getArgs());
 	}
+
+	/**
+	 * 
+	 */
+	public LogAspect() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
 
 }
